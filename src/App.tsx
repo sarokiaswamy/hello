b3e088d2ad806ced1ae807b126aa0c4d6ca43a22
arokiaswamy <mail.9845115777@gmail.com>
2025-12-28 16:34:31 +0530

    Changed title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    Its my day
+    Its my first day
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
